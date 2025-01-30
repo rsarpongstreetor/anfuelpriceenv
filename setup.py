8b5abe1e80c538eb5e7fc6148bc18d6c04aab8ff
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-01-31 05:10:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
 from setuptools import setup, find_packages
-from DataDic.py import DataDicLoader  # Import the class
+import torch
 
-     setup(
-         name='anfuelpriceenv',  # Package name
-         version='0.1.0',  # Package version
-         packages=find_packages(),  # Include all sub-packages
-         install_requires=[  # List of dependencies
-             'torch',
-             'torchrl',
-             'tensordict',
-             'pandas',
-             'numpy',
-             # Add other necessary packages
-         ],
-          loader = DataDicLoader()  # Create an instance of the class
-          DataDic.pt= loader.load_data()  # Call the load_data method to load the data
-          
-          include_package_data=True, # Include the DataDic.pt file
-         
-          package_data={
-            'anfuelpriceenv': ['DataDic.pt'],
-        },
-     )
+# Import the DataDicLoader class
+from anfuelpriceenv.DataDic import DataDicLoader
+
+# Load the data using DataDicLoader
+loader = DataDicLoader()
+DataDic = loader.load_data()
+
+setup(
+    name='anfuelpriceenv',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchrl',
+        'tensordict',
+        'pandas',
+        'numpy',
+        'requests'
+    ],
+    include_package_data=True,  # Ensure package data is included
+    package_data={
+        'anfuelpriceenv': ['DataDic.pt'],  # You might still need this if the file exists
+    },
+    # Function to be executed during installation
+    data_files=[
+        ('anfuelpriceenv/data', ['temp_file.pt']) # Include the downloaded file as data
+    ]
+)
+
+import os
+# Remove the temp file if it's not part of your package data
+os.remove("temp_file.pt")
