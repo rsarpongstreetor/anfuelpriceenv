4369fba7480fddedea5ad286c50ef25c76e7ab36
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-02-02 01:18:09 +0800

    Update anfuelpriceenv.py

--- anfuelpriceenv.py
+++ anfuelpriceenv.py
@@ -52,7 +52,7 @@ os.remove("temp_file.pt")
 
 
 # Access DDataenv from the loaded DataDic (assuming DDataenv is a key in DataDic)
-DDataenv =np.array(DDataenv)
+DDataenv =pd.DataFrame(DDataenv)
 DDataenv=DataDic(DDataenv)
 
 # ... (rest of the code)
