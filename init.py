ddb0a26aa7a164decb1917a1a704ba36ef915f2a
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-02-08 08:44:03 +0800

    Update init.py

--- init.py
+++ init.py
@@ -1,4 +1,10 @@
 # Import only the essential classes or functions
 from .anfuelpriceenv import AnFuelpriceEnv 
+from .anfuelpriceenv import task_1py 
+from .anfuelpriceenv import task_2py 
+from .anfuelpriceenv import task_1 
+from .anfuelpriceenv import task_2
+from .anfuelpriceenv import common 
+
 
 __all__ = ["AnFuelpriceEnv"]
