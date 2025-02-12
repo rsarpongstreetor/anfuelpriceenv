25a75b1c6b0fbd0821fbb632a94369f8da05d9e6
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-02-13 00:45:20 +0800

    Update task_1.py
    Signed-off-by: Manu  Nana Yaw  <59754260+rsarpongstreetor@users.noreply.github.com>

--- task_1.py
+++ task_1.py
@@ -1,10 +1,15 @@
+
 from dataclasses import dataclass, MISSING 
 
 
 @dataclass
 class TaskConfig:
-    max_steps: int =100
+    max_steps: int =10
     supports_continuous_actions:bool=True
     supports_discrete_actions: bool=True
+    seed: int=5
+    num_envs: int=1
+    
+    
 
      
