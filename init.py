34a1878c44db560092fd11b3fbb0c6e6e3448755
Manu  Nana Yaw <59754260+rsarpongstreetor@users.noreply.github.com>
2025-09-01 12:13:13 +0800

    Update init.py
    added import MultiCategorical_Distribution
    
    Signed-off-by: Manu  Nana Yaw  <59754260+rsarpongstreetor@users.noreply.github.com>

--- init.py
+++ init.py
@@ -5,8 +5,10 @@ from .anfuelpriceenv import task_2.py
 from .anfuelpriceenv import task_1.yaml
 from .anfuelpriceenv import task_2.yaml
 from .anfuelpriceenv import common.CustomEnvTask
+from .anfuelpriceenv import MultiCategorical_Distribution
 
 
 
 
 __all__ = ["AnFuelpriceEnv"]
+
